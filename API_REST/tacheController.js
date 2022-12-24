56470d68f08d0cf147aa376803415dbcfe4d9e87
Femure <maxime.femery@gmail.com>
2022-12-24 14:03:37 +0100

    BugFix : liste propre à chaque session

--- API_REST/tacheController.js
+++ API_REST/tacheController.js
@@ -1,3 +1,4 @@
+const session = require('express-session');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const url = "mongodb://127.0.0.1:27017/";
@@ -6,7 +7,7 @@ exports.tacheGet = async function (req, res) {
     try {
         db = await MongoClient.connect(url);
         let dbo = db.db("taches");
-        let datas = await dbo.collection("taches").find({}).toArray();
+        let datas = await dbo.collection(req.session.user).find({}).toArray();
         res.status(200).json(datas);
     } catch (err) {
         console.log(err);
@@ -19,7 +20,7 @@ exports.tachePost = async function (req, res, next) {
     try {
         db = await MongoClient.connect(url);
         let dbo = db.db("taches");
-        await dbo.collection("taches").insertOne(tache);
+        await dbo.collection(req.session.user).insertOne(tache);
         res.status(200).json(tache);
     } catch (err) {
         console.log(err);
@@ -31,7 +32,7 @@ exports.tacheDelete = async function (req, res, next) {
     try {
         db = await MongoClient.connect(url);
         let dbo = db.db("taches");
-        await dbo.collection("taches").deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
+        await dbo.collection(req.session.user).deleteOne({ _id: new mongodb.ObjectId(req.params.id) });
         res.status(200).send();
     } catch (err) {
         console.log(err);
@@ -43,7 +44,7 @@ exports.tachePut = async function (req, res, next) {
     try {
         db = await MongoClient.connect(url);
         let dbo = db.db("taches");
-        await dbo.collection("taches").updateOne({ _id: new mongodb.ObjectId(req.params.id) }, { $set: { titre: req.body.titre, termine: req.body.termine, statut: req.body.statut } });
+        await dbo.collection(req.session.user).updateOne({ _id: new mongodb.ObjectId(req.params.id) }, { $set: { titre: req.body.titre, termine: req.body.termine, statut: req.body.statut } });
         res.status(200).send();
     } catch (err) {
         console.log(err);
